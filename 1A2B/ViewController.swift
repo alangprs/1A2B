6a24433160ae64fdf08aa0d994d82f3838644f2c
will <alangprs1@gmail.com>
2021-02-04 21:10:49 +0800

    Initial Commit

---
+++ 1A2B/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1A2B
+//
+//  Created by 翁燮羽 on 2021/2/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
